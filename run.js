1446116981400c2fbe0fbd85dec1a3ad408929c6
Julien Prugne <julien.prugne@gmail.com>
2017-10-28 18:34:06 -0400

    First try at chord progression installation

--- run.js
+++ run.js
@@ -6,8 +6,8 @@ const tone = require('tonegenerator')
 // tool
 const { randomItemInArray } = require('./tools/meh.js')
 const speaker = require('./tools/speaker');
-const { noteFrequency, scale } = require('./tools/scaleGenerator');
-const { speed } = require('./tools/speed');
+const { notes, scaleOnXOctaves, patterns } = require('./tools/scaleGenerator');
+const { speed, getCurrentFrequency } = require('./tools/speed');
 
 const SHAPES = [
   "sine",
@@ -19,20 +19,45 @@ const SHAPES = [
 const mappedKeyInScale = (key, scale) => {
   return scale[key] ? scale[key] : randomItemInArray(scale)
 }
+
+const ApintaMinor = scaleOnXOctaves(2, patterns.diminished, notes.A4);
+const DpintaMinor = scaleOnXOctaves(2, patterns.diminished, notes.D4);
+const EpintaMajor = scaleOnXOctaves(2, patterns.diminished, notes.E4);
+
+const chordsProgression = [
+  ApintaMinor,
+  ApintaMinor,
+  DpintaMinor,
+  DpintaMinor,
+  EpintaMajor,
+  EpintaMajor
+]
+
+let beat = -1
+let mesure = 0;
+let scale = chordsProgression[mesure]
 ioHook.on(
   "keydown",
   event => {
-    const freq = mappedKeyInScale(event.rawcode, scale);
+    beat += 1
+    if (beat % 3 === 0 ) {
+      beat = 0
+      if (mesure === chordsProgression.length-1) {
+        mesure = 0
+      } else mesure += 1;
+    }
+    const freq = mappedKeyInScale(event.rawcode, chordsProgression[mesure]);
+
     const sound = tone({
       freq,
       lengthInSecs: speed(),
       volume: tone.MAX_16,
       sampleRate: 44100,
       shape: 'sine'
-    });
+    })
 
     const data = Int16Array.from(sound)
-    const buffer = new Buffer(data.length*2);
+    const buffer = new Buffer(data.length*2)
     data.forEach(function (value, index) {
       buffer.writeInt16LE(value, index * 2)
     })
