c8395e4f068e2237325a79303e600e296fa57b33
Julien Prugne <julien.prugne@gmail.com>
2017-10-28 18:34:25 -0400

    tweak speed

--- tools/speed.js
+++ tools/speed.js
@@ -1,18 +1,20 @@
 let baseSpeed = 1;
 let touch = 1;
 let start = new Date;
+let currentFrequency;
 
 const speed = () => {
-  frequency = ++touch / (new Date - start) * 1000
-  const s = baseSpeed / frequency
-  if (s === 0.3) {
+  currentFrequency = ++touch / (new Date - start) * 1000
+  const s = baseSpeed / currentFrequency
+  if (s === 0.4) {
     start = new Date
     touch = 5
   }
-  return s > 0.3 ? 0.3 : s
+  return s > 0.4 ? 0.4 : s
 }
 
-
+const getCurrentFrequency = ()=> currentFrequency
 module.exports = {
-  speed
+  speed,
+  getCurrentFrequency
 }
