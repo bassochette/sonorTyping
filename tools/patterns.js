4291568805676034a8e6e5786de81b562b819d4d
Julien Prugne <julien.prugne@gmail.com>
2017-10-28 16:57:10 -0400

    extract pattern to their own file and create notes

---
+++ tools/patterns.js
@@ -0,0 +1,19 @@
+const minor = [2,1,2,2,1,2,2]
+const major = [2,2,1,2,2,2,1]
+const pintaMajor = [2,2,3,2,3]
+const pintaMinor = [1,2,2,3,1]
+const diminished = [2,1,2,1,2,1,2,1]
+const harmoniqueMinor = [2,1,2,2,1,2,2]
+const melodiqueMinor = [2,1,2,2,2,2,1]
+const blues = [3,1,2,2,3,2]
+
+module.exports = {
+  minor,
+  major,
+  pintaMajor,
+  pintaMinor,
+  diminished,
+  harmoniqueMinor,
+  melodiqueMinor,
+  blues
+}
--- tools/scaleGenerator.js
+++ tools/scaleGenerator.js
@@ -1,17 +1,35 @@
 const noteFrequency = (noteNumber) => Math.pow(2, (noteNumber-9)/12) * 432
+const patterns = require('./patterns')
 
-// Interval Pattern
-const minor = [1,0.5,1,1,0.5,1,1]
-const major = [1,1,0.5,1,1,1,0.5]
-const pintaMajor = [1,1,1.5,1,1.5]
-const pintaMinor = [0.5,1,1,1.5,0.5]
-const diminished = [1,0.5,1,0.5,1,0.5,1,0.5]
-const harmoniqueMinor = [1,0.5,1,1,0.5,1,1]
-const melodiqueMinor = [1,0.5,1,1,1,1,0.5]
-const blues = [1.5,0.5,1,1,1.5,1]
+const notes = {
+  'C4': -12,
+  'C#4': -11,
+  'D4': -10,
+  'D#4': -9,
+  'E4': -8,
+  'F4': -7,
+  'F#4': -6,
+  'G4': -5,
+  'G#4': -4,
+  'A4': -3,
+  'A#4': -2,
+  'B4': -1,
+  'C5': 0,
+  'C#5': 1,
+  'D5': 2,
+  'D#5': 3,
+  'E5': 4,
+  'F5': 5,
+  'F#5': 6,
+  'G5': 7,
+  'G#5': 8,
+  'A5': 9,
+  'A#5': 10,
+  'B5': 11,
+  'C6': 12
+};
 
-const scaleOnXOctaves = (x, pattern) => {
-  let base = -10
+const scaleOnXOctaves = (x, pattern, base = 0) => {
   let scale = [noteFrequency(base)]
   for (x; x!==0; x--) {
       pattern.forEach(
@@ -28,10 +46,12 @@ const scaleOnXOctaves = (x, pattern) => {
   return scale
 }
 
-
-const scale = scaleOnXOctaves(3, pintaMajor)
+const scale = scaleOnXOctaves(2, patterns.pintaMajor, notes.C4);
 
 module.exports = {
   noteFrequency,
+  scaleOnXOctaves,
+  notes,
+  patterns,
   scale
 }
