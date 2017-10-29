950cb377e4918c476ac142aa9ddd59b68e4275ba
Julien Prugne <julien.prugne@gmail.com>
2017-10-29 16:49:01 -0400

    start on lower notes

--- partitions/lesYeuxNoirs.js
+++ partitions/lesYeuxNoirs.js
@@ -1,10 +1,10 @@
 const { notes, scaleOnXOctaves, patterns } = require('../tools/scaleGenerator')
 
 // const ApintaMinor = scaleOnXOctaves(2, patterns.minor, notes.A4);
-const Amajor = scaleOnXOctaves(2, patterns.major, notes['A4'])
-const DminDiminished = scaleOnXOctaves(2, patterns.diminished, notes['D4'])
-const AsharpPintaMajor = scaleOnXOctaves(2, patterns.pintaMajor, notes['A#4'])
-const Gm = scaleOnXOctaves(2, patterns.minor, notes['G5'])
+const Amajor = scaleOnXOctaves(2, patterns.major, notes['A3'])
+const DminDiminished = scaleOnXOctaves(2, patterns.diminished, notes['D3'])
+const AsharpPintaMajor = scaleOnXOctaves(2, patterns.pintaMajor, notes['A#3'])
+const Gm = scaleOnXOctaves(2, patterns.minor, notes['G3'])
 
 const lesYeuxNoirs = [
     Amajor,
--- tools/scaleGenerator.js
+++ tools/scaleGenerator.js
@@ -2,6 +2,18 @@ const noteFrequency = (noteNumber) => Math.pow(2, (noteNumber-9)/12) * 432
 const patterns = require('./patterns')
 
 const notes = {
+  'C3': -25,
+  'C#3': -23,
+  'D3': -22,
+  'D#3': -21,
+  'E3': -20,
+  'F3': -19,
+  'F#3': -18,
+  'G3': -17,
+  'G#3': -16,
+  'A3': -15,
+  'A#3': -14,
+  'B3': -13,
   'C4': -12,
   'C#4': -11,
   'D4': -10,
